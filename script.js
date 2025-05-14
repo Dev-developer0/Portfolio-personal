3592ff466f8a4b8aee38a02baea1af24f3af86a1
Dev-developer0 <dev580076@gmail.com>
2025-05-14 11:26:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Set the current year in the footer
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+// Smooth Scroll for Navbar Links
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 60,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
